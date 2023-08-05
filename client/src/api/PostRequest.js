edad523a96be3d83cf90d6bbe587dfa268cf1a57
Harsh Raj <harshr784@gmail.com>
2023-08-05 09:04:21 +0530

    add api

--- client/src/api/PostRequest.js
+++ client/src/api/PostRequest.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 
-const API = axios.create({ baseURL: 'http://localhost:5000' });
+const API = axios.create({ baseURL: 'https://api-social-media-wf62.onrender.com' });
 
 // API.interceptors.request.use((req) => {
 //     if (localStorage.getItem('profile')) {
--- client/src/api/UploadRequest.js
+++ client/src/api/UploadRequest.js
@@ -9,7 +9,7 @@
 
 import axios from "axios";
 
-const API = axios.create({ baseURL: "http://localhost:5000" });
+const API = axios.create({ baseURL: "https://api-social-media-wf62.onrender.com" });
 
 
 export const uploadImage = (data) => API.post("/upload/", data);
